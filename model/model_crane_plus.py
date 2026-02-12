383135045a5e85c9a9c5715b32b579f5c3fd30e0
YangXiang <xyang6167@gmail.com>
2026-02-12 17:01:29 +0800

    first commit

---
+++ model/model_crane_plus.py
@@ -0,0 +1,11 @@
+import math
+import torch
+import torch.nn.init as init
+import torch.nn.functional as F
+from torch import nn
+from transformers.activations import ACT2FN
+from typing import Optional, Tuple, List, Union
+from transformers import PreTrainedModel, GenerationMixin, PretrainedConfig
+from transformers.modeling_outputs import CausalLMOutputWithPast
+from .config import SpongeBobConfig
+
